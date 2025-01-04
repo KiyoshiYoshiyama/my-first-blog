179b2c4ff0477f517571363db3cce663d62325b6
kyoshiyama <kiyoshi.yoshiyama@gmail.com>
2025-01-04 19:41:42 +0900

    Application extendedpplication extendedd

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e2%4oqbd&)v)(y%5pxcjg0v(ivf!kjsxhu0i((ztjk2qckdq%s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
